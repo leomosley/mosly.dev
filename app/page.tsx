7d3bb0c217b396d0c692a90ffbda032c2a898bcb
leomosley <leo.mosley2@gmail.com>
2024-03-21 22:02:19 +0000

    updated intro section on home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,13 @@ import Blogs from '@/components/Blogs';
 export default async function Home() {
   return (
     <>
-      <section id="introduction" className="mt-8">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <section id="intro" className="mt-8">
+        <p>
+          Hi there, I&apos;m <b>Leo</b>. 
+          I&apos;m a 19 y/o Software Engineering student at <a target="_blank" href="https://www.port.ac.uk/"><b>UoP</b></a>.
+          I&apos;ve have a love for <b>cooking</b> and <b>building things</b>.
+          I enjoy <b>web development</b>, creating fun <b>APIs</b> with <b>Rust</b>, and everything else <b>code</b>. 
+        </p>
       </section>
       <Projects />
       <Blogs />

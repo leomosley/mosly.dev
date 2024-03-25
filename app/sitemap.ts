2de80842d2736391bdeb8dea5d99c9ff3617e9dc
leomosley <leo.mosley2@gmail.com>
2024-03-25 21:11:26 +0000

    fixed site map

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,10 +1,10 @@
-import { blogs } from "@/blog";
+import { getBlogs } from "@/blog";
 
 export default async function Sitemap() {
   const url = "https://leomosley.com";
   const blogPath = "/blog/";
   
-  const blogMap = blogs.map((blog) => ({
+  const blogMap = getBlogs().map((blog) => ({
     url: blogPath + blog.data.filename.slice(0, -3),
     lastModified: blog.data.date
   }));

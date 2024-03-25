1ab82f941b40ab8c239f2852d2ab74da82638e3c
leomosley <leo.mosley2@gmail.com>
2024-03-25 18:32:53 +0000

    added exception handling in getFiles()

--- blog/index.ts
+++ blog/index.ts
@@ -28,10 +28,15 @@ export function getBlog(path: string) {
 }
 
 export function getFiles() {
-  const files = fs.readdirSync('./blog'); 
-  const index = files.findIndex((x) => x === 'index.ts');
-  files.splice(index, 1);
-  return files;
+  try {
+    const files = fs.readdirSync('./blog'); 
+    const index = files.findIndex((x) => x === 'index.ts');
+    files.splice(index, 1);
+    return files;
+  } catch (error) {
+    console.error('Error reading blog directory:', error);
+    return [];
+  }
 }
 
 export const files = getFiles();

572489a7a8e305afc4e7a1f43cd90648ca37f5a7
leomosley <leo.mosley2@gmail.com>
2024-10-30 19:09:58 +0000

    fix: update linkedin

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,16 +3,17 @@ import { FaGithub, FaLinkedin, FaXTwitter, FaSitemap, FaRss } from 'react-icons/
 export function Footer() {
   const url = process.env.VERCEL_URL
     ? 'https://' + process.env.VERCEL_URL
-    : 'http://localhost:3000'
-    ;
+    : 'http://localhost:3000';
+
   const links = [
     { href: `${url}/sitemap.xml`, icon: <FaSitemap /> },
     { href: `${url}/feed.xml`, icon: <FaRss /> },
     { href: 'https://github.com/leomosley', icon: <FaGithub /> },
-    { href: 'https://www.linkedin.com/in/leo-mosley-606785222/', icon: <FaLinkedin /> },
+    { href: 'https://www.linkedin.com/in/leo-mosley-22b7b229b/', icon: <FaLinkedin /> },
     { href: 'https://twitter.com/leomosly', icon: <FaXTwitter /> },
 
   ];
+
   return (
     <footer id='footer' className='flex border-t-2 mt-20 mb-4 border-neutral-400'>
       <div className='flex gap-4 ml-auto mt-4 text-neutral-400'>

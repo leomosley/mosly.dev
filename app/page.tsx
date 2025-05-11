179fec062f6e767f9f902d9a6b5ba29bba8d4311
leomosley <leo.mosley2@gmail.com>
2025-05-11 15:28:42 +0100

    feat: updated bio

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,9 @@ export default async function Home() {
       <section id='intro' className='mt-8'>
         <p>
           Hi there, I&apos;m <b>Leo</b>.
-          I&apos;m a Software Engineering student at <a target='_blank' href='https://www.port.ac.uk/'><b>UoP</b></a>{" "}
+          I&apos;m a Software Engineering student {"@"} <a target='_blank' href='https://www.port.ac.uk/'><b>UoP</b></a>{" "}
           who&apos;s passionate about <b>coding</b> and <b>building cool things</b>.
-          I enjoy <b>full-stack web development</b>, creating fun <b>APIs</b>, and everything else <b>code</b>.
+          I enjoy <b>full-stack web development</b>, developing <b>CLI tools</b>, and everything else <b>code</b>.
         </p>
       </section>
       <Projects />
